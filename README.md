647cf72c65d673b26bc8226cda4f21e8453378ee
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-02-07 12:17:06 -0700

    Added request to github-server and CLI argument handling

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+## GitHub Star Client
 
+A client that interacts with the github-server API
+
+* `go run main.go mattcullenmeyer/tinytrader mattcullenmeyer/anaplan mattcullenmeyer/github-client`
--- main.go
+++ main.go
@@ -1,29 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
-// https://levelup.gitconnected.com/tutorial-how-to-create-a-cli-tool-in-golang-a0fd980264f
+type github struct {
+	Repository string `json:"Repository"`
+	Stars      string `json:"Stars"`
+}
 
-func getArgs() []string {
+// https://levelup.gitconnected.com/tutorial-how-to-create-a-cli-tool-in-golang-a0fd980264f
+// https://www.geeksforgeeks.org/command-line-arguments-in-golang/
+
+func getData(repo string) string {
+
+	url := fmt.Sprintf("http://localhost:8080/api?repo=%s", repo)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := github{}
+	jsonErr := json.Unmarshal(body, &result)
+	if jsonErr != nil {
+		log.Fatal(err)
+	}
+
+	return result.Stars
+}
+
+func main() {
+
 	if len(os.Args) < 2 {
 		fmt.Println("At least one repository is required")
 	}
 
 	repos := os.Args[1:]
 
-	return repos
-
-}
-
-func main() {
-
-	repos := getArgs()
-
 	for _, repo := range repos {
-		fmt.Println(repo)
+		stars := getData(repo)
+		fmt.Printf("%s --> %s\n", repo, stars)
 	}
 
 }
