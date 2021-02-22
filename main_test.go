3b4c4dbc4be1eaec01f9e486c6d036420dd7a7e4
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-02-22 15:52:27 -0700

    Updated URL path for test

--- main_test.go
+++ main_test.go
@@ -18,31 +18,26 @@ func TestCliResponse(t *testing.T) {
 			name: "no arguments",
 			args: []string{},
 			want: "At least one <organization>/<repository>",
-			url:  "http://192.168.49.2:30893",
 		},
 		{
 			name: "invalid argument",
 			args: []string{"abcdef"},
 			want: "The format should be <organization>/<repository>",
-			url:  "http://192.168.49.2:30893",
 		},
 		{
 			name: "invalid repo",
 			args: []string{"abc/def"},
 			want: "not a valid public GitHub repository",
-			url:  "http://192.168.49.2:30893",
 		},
 		{
 			name: "one valid repo",
 			args: []string{"mattcullenmeyer/anaplan"},
 			want: "mattcullenmeyer/anaplan -->",
-			url:  "http://192.168.49.2:30893",
 		},
 		{
 			name: "two valid repos",
 			args: []string{"mattcullenmeyer/anaplan", "mattcullenmeyer/tinytrader"},
 			want: "mattcullenmeyer/tinytrader -->",
-			url:  "http://192.168.49.2:30893",
 		},
 	}
 	// Loop through each subtest
@@ -52,12 +47,8 @@ func TestCliResponse(t *testing.T) {
 			// Simulate passing arguments
 			// https://stackoverflow.com/questions/33723300/how-to-test-the-passing-of-arguments-in-golang
 			os.Args = tc.args
-			//urlPtr := flag.String("url", "http://localhost:8080", "url path")
-			//flag.Parse()
-			//url := *urlPtr
-			//repos := flag.Args()
 
-			txt := cliResponse(os.Args, tc.url)
+			txt := cliResponse(os.Args, "http://localhost:8080")
 
 			if !strings.Contains(txt, tc.want) {
 				t.Errorf("%s response doesn't include '%s' as expected; found %s", tc.name, tc.want, txt)
